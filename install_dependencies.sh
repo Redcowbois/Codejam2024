52e8494b3a3b9c95fd380d976c9a6d06f3ac4e5a
root <root@LAPTOP-9TGODEP3.localdomain>
2024-11-23 13:28:01 -0500

    textgen test

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,3 +2,5 @@ pip install --upgrade pip
 pip install django
 pip install --upgrade transformers datasets[audio] accelerate
 pip install flash-attn --no-build-isolation
+pip install requests
+pip install huggingface_hub

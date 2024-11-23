cb4f504259991e712f8a3fd43c30ea5aaf6de56c
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2024-11-23 13:29:04 -0500

    added transcibe audio function

--- app/codejam2024/in_models.py
+++ app/codejam2024/in_models.py
@@ -1,13 +1,13 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from utils import ROOT_DIR
+from os import path
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 print(f"Using {device} for processing!")
 
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
-attn_implementation = "flash_attention_2" if torch.cuda.is_available() else "sdpa"
 
 model_id = "openai/whisper-large-v3-turbo"
 
@@ -16,7 +16,6 @@ model = AutoModelForSpeechSeq2Seq.from_pretrained(
     torch_dtype=torch_dtype,
     low_cpu_mem_usage=True,
     use_safetensors=True,
-    attn_implementation=attn_implementation,
 )
 
 model.to(device)
@@ -39,3 +38,19 @@ pipe = pipeline(
 generate_kwargs = {
     "language": "english",
 }
+
+
+def transcribe_to_file(mp3_path: str, file_path: str):
+    with open(file_path, "a+") as file:
+        result = pipe(
+            mp3_path,
+            generate_kwargs=generate_kwargs,
+            return_timestamps=True,
+        )
+        file.write(result["text"])
+
+
+transcribe_to_file(
+    path.join(ROOT_DIR, "sample_audio", "sample1.mp3"),
+    path.join(ROOT_DIR, "sample_audio", "sample1.txt"),
+)
---
+++ app/codejam2024/utils.py
@@ -0,0 +1,5 @@
+from os import path
+
+ROOT_DIR = path.dirname(path.abspath(__file__))
+
+print(ROOT_DIR)  # path to codejam2024 dir

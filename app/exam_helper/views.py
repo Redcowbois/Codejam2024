d93afc793418bc47d158b51b396a9374bff7b753
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2024-11-24 06:04:31 -0500

    added file conversion

--- app/exam_helper/views.py
+++ app/exam_helper/views.py
@@ -8,69 +8,95 @@ import json
 import exam_helper.multiple_choice
 import exam_helper.qwen
 from os import path
+from exam_helper.utilities import convert_mp4_to_mp3, extract_text_from_pdf
+from exam_helper.whisper import get_whisper_pipe
 
-def handle_uploaded_file(f):
-     with open(path.join(ROOT_DIR, "uploaded_files", f.name), "wb+") as file:
-          for chunk in f.chunks():
-               file.write(chunk)
+
+def handle_uploaded_file(f, file_path):
+    with open(file_path, "wb+") as file:
+        for chunk in f.chunks():
+            file.write(chunk)
+
 
 @csrf_exempt
 def index(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES["file"])
-            return JsonResponse({'redirect_url': 'options'})
+            file_path = path.join(
+                ROOT_DIR, "uploaded_files", request.FILES["file"].name
+            )
+            handle_uploaded_file(request.FILES["file"], file_path)
+            filename, extension = path.splitext(file_path)
+            data = ""
+            if extension == ".mp4":
+                convert_mp4_to_mp3(file_path, f"{filename}.mp3")
+                extension = ".mp3"
+            if extension == ".mp3":
+                pipe = get_whisper_pipe()
+                data = pipe(f"{filename}.mp3")["text"]
+                del pipe
+            if extension != ".pdf":
+                with open("data.txt", "wb+") as file:
+                    file.write(data)
+            else:
+                extract_text_from_pdf(file_path, "data.txt")
+
+            return JsonResponse({"redirect_url": "options"})
 
         # Return the response with the redirection URL
-        return JsonResponse({'redirect_url': 'options'})
-    
-    if request.method == 'GET':
-        template = loader.get_template('main.html')
+        return JsonResponse({"redirect_url": "options"})
+
+    if request.method == "GET":
+        template = loader.get_template("main.html")
         return HttpResponse(template.render())
+
 
 @csrf_exempt
 def options(request):
-        if request.method == 'POST':
-            print("options received")
-        template = loader.get_template('file-options.html')
-        return HttpResponse(template.render())
+    if request.method == "POST":
+        print("options received")
+    template = loader.get_template("file-options.html")
+    return HttpResponse(template.render())
 
 
 @csrf_exempt
 def summary(request):
-        if request.method == 'POST':
-            print("options received")
-        template = loader.get_template('Summary.html')
-        return HttpResponse(template.render())
+    if request.method == "POST":
+        print("options received")
+    template = loader.get_template("Summary.html")
+    return HttpResponse(template.render())
+
 
 @csrf_exempt
 def flashcard(request):
-        if request.method == 'POST':
-            print("options received")
-        template = loader.get_template('flashcard.html')
-        return HttpResponse(template.render())
+    if request.method == "POST":
+        print("options received")
+    template = loader.get_template("flashcard.html")
+    return HttpResponse(template.render())
+
 
 @csrf_exempt
 def podcast(request):
-        if request.method == 'POST':
-            print("options received")
-        template = loader.get_template('podcast.html')
-        return HttpResponse(template.render())
+    if request.method == "POST":
+        print("options received")
+    template = loader.get_template("podcast.html")
+    return HttpResponse(template.render())
+
 
 @csrf_exempt
 def quiz(request):
-        if request.method == 'POST':
-            print("options received")
-        template = loader.get_template('quiz.html')
-        return HttpResponse(template.render())
+    if request.method == "POST":
+        print("options received")
+    template = loader.get_template("quiz.html")
+    return HttpResponse(template.render())
 
 
 @csrf_exempt
 def trial(request):
-    if request.method == 'POST':
-        a = request.POST['message']
+    if request.method == "POST":
+        a = request.POST["message"]
         return HttpResponse(a)
-    
-    template = loader.get_template('trial.html')
+
+    template = loader.get_template("trial.html")
     return HttpResponse(template.render())

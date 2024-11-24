026c220de560d5c38eb870e6b763d100d05f1d3b
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2024-11-24 01:41:19 -0500

    wrote test for flashcards

--- app/codejam2024/flashcards.py
+++ app/codejam2024/flashcards.py
@@ -1,4 +1,6 @@
-from qwen import initialize_flashcard_maker_chat
+from qwen import initialize_flashcard_maker_chat, get_qwen_pipe
+from utils import ROOT_DIR, read_file_into_string
+from os import path
 
 
 def generate_flashcards_for_info(info: str, pipe):
@@ -6,30 +8,40 @@ def generate_flashcards_for_info(info: str, pipe):
     chat.append(
         {"role": "user", "content": f"This is the information you will use: {info}"}
     )
-    cards_contents = []
-    for _ in range(5):
-        chat.append(
-            {
-                "role": "user",
-                "content": "Create a new, unique question. Only give the question, no answer.",
-            }
+    chat.append(
+        {
+            "role": "user",
+            "content": "Create a new, unique question. Only give the question, no answer.",
+        }
+    )
+    response = pipe(chat, max_new_tokens=100, temperature=0.2)
+
+    question = response[0]["generated_text"][-1]["content"]
+
+    chat = response[0]["generated_text"]
+
+    chat.append(
+        {
+            "role": "user",
+            "content": "Now give the correct answer to the last question.",
+        }
+    )
+
+    response = pipe(chat, max_new_tokens=100, temperature=0.2)
+
+    answer = response[0]["generated_text"][-1]["content"]
+    return f"{question}; {answer}"
+
+
+info = read_file_into_string(path.join(ROOT_DIR, "sample_audio", "sample1.txt"))
+
+pipe = get_qwen_pipe()
+
+print(
+    "\n".join(
+        generate_flashcards_for_info(
+            info[int(len(info) * i / 3) : int(len(info) * (i + 1) / 3)], pipe
         )
-        response = pipe(chat, max_new_tokens=100, temperature=0.2)
-
-        question = response[0]["generated_text"][-1]["content"]
-
-        chat = response[0]["generated_text"]
-
-        chat.append(
-            {
-                "role": "user",
-                "content": "Now give the correct answer to the last question.",
-            }
-        )
-
-        response = pipe(chat, max_new_tokens=100, temperature=0.2)
-
-        answer = response[0]["generated_text"][-1]["content"]
-
-        cards_contents.append(f"{question}; {answer}")
-    return "\n".join(cards_contents)
+        for i in range(3)
+    )
+)
